b64b36256d45fb992b76cd892ec03dde8ff2f9e0
phil294 <philip@waritschlager.de>
2022-12-18 20:26:12 +0100

    fix clipboard via tkbash

--- clpChooser.sh
+++ clpChooser.sh
@@ -5,13 +5,13 @@
 set -e
 
 THISDIR="$(dirname "$(readlink -f "$0")")"
-db="${THISDIR}/clps"
+db="$1"
 char() { printf "\x$(printf %x $1)" ;}
 
 # store current first
-"${THISDIR}/storeClp.sh"
+"${THISDIR}/storeClp.sh" "$db"
 
-tkbash clpchooser window -w 1300 -h 370 --title "Clipboard Chooser"
+tkbash clpchooser -w 1300 -h 415 --title "Clipboard Chooser"
 
 IFS=$'\n'
 clpsLast=($(sqlite3 "$db" "SELECT replace(clp, char(10), 'b926ac9569e69458a5c676eecf83bbf2') FROM clps ORDER BY id DESC LIMIT 26"))
@@ -22,10 +22,10 @@ add() {
 	local clp="${clpsLast[$i]//b926ac9569e69458a5c676eecf83bbf2/$'\n'}"
 	local truncated="$(sed -E 's/(.{180}).+/\1.../' <<<"${clp//$'\n'/\\n}")" # >180 chars: ellipsis... / line break to \n-string
 	# v escape single quotes: a'b -> a'"'"'b
-	tkbash clpchooser window --hotkey "$hotkey" --command "
-		echo -n '${clp//\'/\'\"\'\"\'}' |xsel -ib
-		tkbash clpchooser window --quit"
-	local extra_space="$([[ $((i%2)) == 0 ]] && echo '   ')"
+	tkbash clpchooser --hotkey "$hotkey" --command "
+		echo -n '${clp//\'/\'\"\'\"\'}' |xclip -sel c
+		tkbash clpchooser --quit"
+	local extra_space="$([[ $((i%2)) == 0 ]] && echo '     ')"
 	label="${label} $hotkey   $extra_space $truncated"$'\n'
 }
 
@@ -34,6 +34,6 @@ for i in {0..4}; do add $i; done
 tkbash clpchooser label l1 -x 0 -y 0 -w 1300 -h 75 -t "$label"
 label=""
 for i in {5..25}; do add $i; done
-tkbash clpchooser label l2 -x 0 -y 71 -w 1300 -h 295 -t "$label"
+tkbash clpchooser label l2 -x 0 -y 78 -w 1300 -h 345 -t "$label"
 
-tkbash clpchooser window --hotkey Escape --command "tkbash clpchooser window --quit"
+tkbash clpchooser --hotkey Escape --command "tkbash clpchooser --quit"
--- storeClp.sh
+++ storeClp.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-THISDIR="$(dirname "$(readlink -f "$0")")"
-db="${THISDIR}/clps"
+db="$1"
+
+if [ -z "$db" ]; then
+	echo "no database given" >&2
+	exit 1
+fi
 	
 if ! [ -f "$db" ]; then
 	IFS=$'\n'
 	sqlite3 "$db" "CREATE TABLE clps (id integer primary key autoincrement, datetime integer, clp text);"
-	echo "Created table"
+	echo "Created table" >&2
 fi
 
 clp="$(xclip -sel c -o)"
-[ -z "$clp" ] && echo "clipboard empty / nonstring." && exit 0
+[ -z "$clp" ] && echo "clipboard empty / nonstring." >&2 && exit 0
 truncated="${clp:0:10000}"
 
+newline_mock="b926ac9569e69458a5c676eecf83bbf2"
 IFS=$'\n'
-clpsLast=($(sqlite3 "$db" "SELECT replace(clp, char(10), 'b926ac9569e69458a5c676eecf83bbf2') FROM clps order by id desc limit 26"))
-clpsLast=("${clpsLast[@]//b926ac9569e69458a5c676eecf83bbf2/$'\n'}")
+clpsLast=($(sqlite3 "$db" "SELECT replace(clp, char(10), '$newline_mock') FROM clps order by id desc limit 26"))
+clpsLast=("${clpsLast[@]//$newline_mock/$'\n'}")
 
 for i in "${clpsLast[@]}"; do
-	[[ "$i" == "${truncated//b926ac9569e69458a5c676eecf83bbf2/$'\n'}" ]] && echo "Clipboard \"$truncated\" already in previous 26 clps contained" && exit 0
+	[[ "$i" == "${truncated//$newline_mock/$'\n'}" ]] && echo "Clipboard \"$truncated\" already in previous 26 clps contained" >&2 && exit 0
 done
 
 sqlite3 "$db" "insert into clps (datetime, clp) values (datetime('now'), '${truncated//\'/\'\'}');"
-echo "Inserted clipboard \"$truncated\" successfully."
+echo "Inserted clipboard \"$truncated\" successfully." >&2
+
+exit 0
